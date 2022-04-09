e1b9646e67024bbb20ddfea1cd90a71efacce6b0
kananazizli <kananazizli.ao@gmail.com>
2022-04-09 14:39:47 +0300

    Added 15th Codewars Challenge "Calculate BMI"

---
+++ 8kyu/calculate_bmi/main.js
@@ -0,0 +1,19 @@
+/* Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+function bmi(weight, height) {
+  result = weight / height ** 2;
+  
+  return (result <= 18.5) ? 'Underweight'
+   : (result <= 25) ? 'Normal'
+   :(result <= 30) ? 'Overweight' 
+   : 'Obese';
+}
