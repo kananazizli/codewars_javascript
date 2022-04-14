671ed6c0ee75d607865c928f8310c8de00b79143
kananazizli <kananazizli.ao@gmail.com>
2022-04-14 12:28:43 +0300

    Added 26th Codewars Challenge "Is It a Palindrome?"

---
+++ 8kyu/is_it_a_palindrome/main.js
@@ -0,0 +1,3 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+const isPalindrome = x => (x.toLowerCase() === x.toLowerCase().split('').reverse().join('')) ? true : false;
