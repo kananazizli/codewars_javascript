4e0c086ebcd380e31193c4f356727ba8b75c85d9
kananazizli <kananazizli.ao@gmail.com>
2022-05-27 15:42:15 -0400

    Redacted Challenge instrustions and changed the variable names for better readability

--- 8kyu/basic_variable_assignment/main.js
+++ 8kyu/basic_variable_assignment/main.js
@@ -1,5 +1,11 @@
-// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+let firstPart = "code";
+let secondPart = "wa.rs";
+let name = firstPart + secondPart;
 
-var a = "code";
-var b = "wa.rs";
-var name = a + b;
+
+/* This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+*/
